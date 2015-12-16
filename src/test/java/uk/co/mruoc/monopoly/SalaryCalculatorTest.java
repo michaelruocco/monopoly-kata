993a18c9d277385f962d92b0aaa95f9dc05bebe0
Michael Ruocco <michael.ruocco@travisperkins.co.uk>
2015-12-16 22:34:30 +0000

    adds unit tests for salary calculator or super tax calculator

---
+++ src/test/java/uk/co/mruoc/monopoly/SalaryCalculatorTest.java
@@ -0,0 +1,18 @@
+package uk.co.mruoc.monopoly;
+
+import org.junit.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+public class SalaryCalculatorTest {
+
+    private static final int SALARY = 200;
+
+    private final SalaryCalculator calculator = new SalaryCalculator();
+
+    @Test
+    public void shouldCalculateSalary() {
+        assertThat(calculator.calculate()).isEqualTo(SALARY);
+    }
+
+}
---
+++ src/test/java/uk/co/mruoc/monopoly/SuperTaxCalculatorTest.java
@@ -0,0 +1,18 @@
+package uk.co.mruoc.monopoly;
+
+import org.junit.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+public class SuperTaxCalculatorTest {
+
+    private static final int SUPER_TAX_CHARGE = 75;
+
+    private final SuperTaxCalculator calculator = new SuperTaxCalculator();
+
+    @Test
+    public void shouldCalculateSuperTaxCharge() {
+        assertThat(calculator.calculate()).isEqualTo(SUPER_TAX_CHARGE);
+    }
+
+}
